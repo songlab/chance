33e292aa78519460a13b7fe938a45d968846a9d5
Aaron Diaz <aad1974@gmail.com>
2014-10-22 23:25:41 -0700

    minor

--- chance_com/fuse.m
+++ chance_com/fuse.m
@@ -4,15 +4,17 @@ function w=fuse(rS)
 %In: rS is a nxm matrix of m IP density col vecs
 %Out: w is the set of weight vectors, ie. the consensus IP is rS*w
 
+if ~matlabpool('size'), matlabpool; end
 R=cov(rS,1);
 [w,~]=eigs(R,[],1,'lm');
 w=w/sum(w);
 if any(w<0),w=-w;end
 if any(w<0)
     r=rand(size(rS,2),1);
-    opts=optimset('Display','off','Algorithm','active-set');
+    opts=optimset('Display','off','Algorithm','active-set','UseParallel','always');
     w=fmincon(@(x)-(x'*R*x),r,[],[],[],[],zeros(size(rS,2),1),[],@(x) cons_fun(x),opts);
 end
 w(w<0)=0;
 if any(isnan(w))|any(isinf(w)),w=ones(size(w))/length(w);
 else, w=w/sum(w);end
+matlabpool close
